677e6fef0643c725a05a1287ea2572ce449710b7
Christopher Variara <cvariara@gmail.com>
2023-12-21 14:16:15 -0500

    Added logic to for messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,48 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+let date = new Date().toLocaleString("en-US", {
+  timeZone: userTimeZone,
+  hour: "2-digit",
+  minute: "2-digit",
+  year: "numeric",
+  month: "numeric",
+  day: "numeric",
+});
+
+const messages = [
+  {
+    text: "Hello World!",
+    user: "Chris",
+    added: date,
+  },
+  {
+    text: "Hello Chris!",
+    user: "Bob",
+    added: date,
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", (req, res, next) => {
+  res.render("index", { title: "Mini Message Board", messages: messages });
+});
+
+router.post("/", (req, res, next) => {
+  messages.push({
+    text: req.body.message,
+    user: req.body.user,
+    added: new Date().toLocaleString("en-US", {
+      timeZone: userTimeZone,
+      hour: "2-digit",
+      minute: "2-digit",
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+    }),
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
