fa58b3f73e59c865e9f618a61a336c32f207bef0
Christopher Variara <cvariara@gmail.com>
2024-02-01 16:01:19 -0500

    Added date-fns to format the date and time

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,10 +1,19 @@
 const Messages = require('../models/messages');
+const { formatDistanceToNow } = require('date-fns');
 
 // get messages
 const getMessages = async (req, res, next) => {
   try {
     const messages = await Messages.find().sort({ timestamp: -1 });
-    res.render("index", { title: "Mini Message Board", messages });
+
+    // Convert timestamp strings to Date objects
+    const formattedMessages = messages.map(message => ({
+      ...message._doc,
+      //timestamp: format(new Date(message.timestamp), 'MM-dd-yyyy HH:mm')
+      timestamp: formatDistanceToNow(new Date(message.timestamp), { addSuffix: true })
+    }));
+
+    res.render("index", { title: "Mini Message Board", messages: formattedMessages });
   } catch (err) {
     next(err);
   }
